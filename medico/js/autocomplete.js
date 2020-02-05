1f25256d2349fb03b4a77bc2d5867ef853d763ec
AmyGourlay <37215255+AmyGourlay@users.noreply.github.com>
2020-02-05 16:16:08 +0000

    Updated Slider functionality

--- medico/js/autocomplete.js
+++ medico/js/autocomplete.js
@@ -10,13 +10,18 @@ $(document).ready(function() {
 
             $( "#slider-range" ).slider({
                 range: true,
-                min: 0,
+                min: response.price_min,
                 max: response.price_max,
-                values: [ 0, response.price_max ],
+                values: [ response.price_min, response.price_max ],
                 slide: function( event, ui ) {
-                    $( "#amount-top" ).val(ui.values[ 0 ]);
-                    $( "#amount-bottom" ).val(ui.values[ 1 ]);
+                    for (var i=0; i<ui.values.length; i++)
+                    {
+                        $("input.amount[data-index=" + i + "]").val(ui.values[i]);
+                    }
+                    //$( "#amount-bottom" ).val(ui.values[ 0 ]);
+                    //$( "#amount-top" ).val(ui.values[ 1 ]);
                 }
+            });
             // }).slider("pips", {
             //     first: "label",
             //     last: "label",
@@ -32,21 +37,42 @@ $(document).ready(function() {
             //     prefix: "",
             //     suffix: ""
             // });
-            });
+            
 
-            $( "#distance-slider-range" ).slider({
+            var dsr = $( "#distance-slider-range" ).slider({
                 value: 200,
                 max: 20000,
                 range: "max",
                 animate: true,
                 slide: function( event, ui ) {
-                    $( "#distance-amount-top" ).val(ui.value + " " + "Miles");
+                    $( "#distance-amount-top" ).val(ui.value);
                 }
             });
+
+            $("input.amount").change(function() {
+                var $this = $(this);
+                
+                console.log("1" + " " + $this);
+                console.log("2 " + $this.data("index"));
+                console.log("3 " + $this.val());
+                console.log("4 " + $("#amount-bottom").val());
+                console.log("5 " + $("#amount-top").val());
+                
+                if (($this.data("index") == 0 && ($this.val() < $("#amount-bottom").val())) || ($this.data("index") == 1 && ($this.val() > $("#amount-top").val())))
+                {
+                    $("#slider-range").slider("values", $this.data("index"), $this.val());
+                }
+                
+            });
+
+            $( "#distance-amount-top" ).on( "keyup", function() {
+                console.log(this.value);
+                dsr.slider( "value", this.value );
+            });
             
-            $( "#amount-top" ).val($( "#slider-range" ).slider( "values", 0 ));
-            $( "#amount-bottom" ).val($( "#slider-range" ).slider( "values", 1 ));
-            $( "#distance-amount-top" ).val($( "#distance-slider-range" ).slider( "value" ));
+            //$( "#amount-top" ).val($( "#slider-range" ).slider( "values", 0 ));
+            //$( "#amount-bottom" ).val($( "#slider-range" ).slider( "values", 1 ));
+            //$( "#distance-amount-top" ).val($( "#distance-slider-range" ).slider( "value" ));
         }
     });
 });
