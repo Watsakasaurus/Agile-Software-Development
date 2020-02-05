a60e5e78a4d0be4cb01b1c2af5f39db5176f551d
Rekfuki <anavasaitis@gmail.com>
2020-02-05 13:59:19 +0000

    Add prefixes to input fields

--- medico/js/autocomplete.js
+++ medico/js/autocomplete.js
@@ -10,13 +10,28 @@ $(document).ready(function() {
 
             $( "#slider-range" ).slider({
                 range: true,
-                min: response.price_min,
+                min: 0,
                 max: response.price_max,
-                values: [ response.price_min, response.price_max ],
+                values: [ 0, response.price_max ],
                 slide: function( event, ui ) {
-                    $( "#amount-top" ).val( "$" + ui.values[ 0 ]);
-                    $( "#amount-bottom" ).val( "$" + ui.values[ 1 ]);
+                    $( "#amount-top" ).val(ui.values[ 0 ]);
+                    $( "#amount-bottom" ).val(ui.values[ 1 ]);
                 }
+            // }).slider("pips", {
+            //     first: "label",
+            //     last: "label",
+            //     rest: "label",
+            //     step: 50000,
+            //     labels: true,
+            //     prefix: "$",
+            //     suffix: ""
+            // }).slider("float", {
+            //     handle: true,
+            //     pips: true,
+            //     labels: true,
+            //     prefix: "",
+            //     suffix: ""
+            // });
             });
 
             $( "#distance-slider-range" ).slider({
@@ -29,9 +44,9 @@ $(document).ready(function() {
                 }
             });
             
-            $( "#amount-top" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ));
-            $( "#amount-bottom" ).val( "$" + $( "#slider-range" ).slider( "values", 1 ));
-            $( "#distance-amount-top" ).val($( "#distance-slider-range" ).slider( "value" ) + "Miles");
+            $( "#amount-top" ).val($( "#slider-range" ).slider( "values", 0 ));
+            $( "#amount-bottom" ).val($( "#slider-range" ).slider( "values", 1 ));
+            $( "#distance-amount-top" ).val($( "#distance-slider-range" ).slider( "value" ));
         }
     });
 });
